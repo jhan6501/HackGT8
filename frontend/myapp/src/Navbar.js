eb5fa63dc373ca75207083706cdb454e5e73cec6
Jerry Han <jhan6501@gmail.com>
2021-10-23 16:26:11 -0400

    added modal

--- frontend/myapp/src/Navbar.js
+++ frontend/myapp/src/Navbar.js
@@ -1,7 +1,26 @@
+import Form from "./Form";
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+
 function Navbar() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const setModalIsOpenToTrue =()=>{
+        setModalIsOpen(true)
+    }
+
+    const setModalIsOpenToFalse =()=>{
+        setModalIsOpen(false)
+    }
+
     return (
         <div>
             Top nav bar
+            <button onClick={setModalIsOpenToTrue}>Click to Open Modal</button>
+            <Modal isOpen = {modalIsOpen}>
+                <button onClick={setModalIsOpenToFalse}>x</button>
+                <Form/>
+            </Modal>
         </div>
     )
 }
