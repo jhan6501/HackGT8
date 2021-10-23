f179bcbb626f75e714b6cf5f674557b7e7ca3c8b
Jerry Han <jhan6501@gmail.com>
2021-10-22 22:54:28 -0400

    made first backend api

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.http import JsonResponse
 
 def index(request):
     print ('test')
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def api_call(request):
+    data = {
+        'data': 'calix is a dumb fucking freshmen xd',
+    }
+
+    return JsonResponse(data)
+
